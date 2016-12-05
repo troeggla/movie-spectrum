a2fe6bf4e279964719689ad14fa99d2951b32da7
Thomas Roeggla (fruchtcomputer15) <tom@lichtfragmente.com>
2016-12-05 18:07:00 +0100

    extracted normal training and crossvalidation into separate functions

--- credits_detection/train_model.py
+++ credits_detection/train_model.py
@@ -1,8 +1,11 @@
 import cv2
 import numpy as np
-from sklearn import svm
-from sklearn.model_selection import train_test_split
+import sys
+
 from pickle import dump
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from time import time
 
 
 def get_frames_from_file(infile):
@@ -40,7 +43,27 @@ def get_dataset(files, target):
     return X, np.full((rows, 1), target, dtype=int)
 
 
+def crossvalidate(model, X, y):
+    scores = cross_val_score(model, X, y, cv=KFold(shuffle=True), scoring="f1")
+
+    print "score statistics:", scores.mean(), scores.std() * 2
+    print "individual scores:", scores
+
+
+def train_and_dump_model(model, X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    model.fit(X_train, y_train)
+    print "score:", model.score(X_test, y_test)
+
+    print "dumping model..."
+    dump(model, open("model.p", "wb"))
+
+
 def main():
+    start = time()
+    print "loading data..."
+
     credits_X, credits_y = get_dataset(
         ["credits/mad_max.mov", "credits/under_the_skin.mov"],
         1
@@ -54,6 +77,7 @@ def main():
     )
 
     print "content done:", content_X.shape, content_y.shape
+    print "took", time() - start, "sec"
 
     X = np.vstack((credits_X, content_X))
     y = np.vstack((credits_y, content_y)).ravel()
